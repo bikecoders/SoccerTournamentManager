242994810f44455cd0cd942b97f17bbda1d904d6
dianjuar <dianjuar@gmail.com>
2018-02-05 20:43:41 -0500

    Create the Team Model
    related #2

---
+++ src/app/shared/person.model.ts
@@ -0,0 +1,16 @@
+export interface Person {
+  /**
+   * Name of the person
+   */
+  name;
+
+  /**
+   * Surname of the person
+   */
+  surname;
+
+  /**
+   * Birth date of the person
+   */
+  birthDate: Date;
+}
