2be5689dfd38ce64b4b5d9a4f9c34f107250b3a6
dianjuar <dianjuar@gmail.com>
2018-02-06 10:37:04 -0500

    Add Fake Teams Data

--- src/app/team/shared/teams.service.ts
+++ src/app/team/shared/teams.service.ts
@@ -7,6 +7,28 @@ export class TeamsService extends CrudList<Team> {
 
   constructor() {
     super();
+
+    this.initFakeData();
+  }
+
+  /**
+   * Init the service with some random values
+   */
+  private initFakeData() {
+    const italyTeam = <Team>{
+      name: 'Italy',
+      flag: 'assets/img/fakeTeams/italy/team/flag.png',
+      shield: 'assets/img/fakeTeams/italy/team/shield.png'
+    };
+
+    const germanyTeam = <Team>{
+      name: 'Germany',
+      flag: 'assets/img/fakeTeams/germany/team/flag.png',
+      shield: 'assets/img/fakeTeams/germany/team/shield.png'
+    };
+
+    this.elements.push(italyTeam);
+    this.elements.push(germanyTeam);
   }
 
 }
