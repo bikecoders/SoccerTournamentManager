7ca85fdf5d77c5d95c0c97439b3e0a1f9094065f
dianjuar <dianjuar@gmail.com>
2018-02-05 23:06:22 -0500

    Create Team Boiler Plate Service
    Related #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { TeamModule } from './team/team.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     BrowserAnimationsModule,
     SharedModule,
+    TeamModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/team/team.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TeamsComponent } from './teams/teams.component';
+import { TeamsService } from './shared/teams.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TeamsComponent],
+  providers: [TeamsService]
+})
+export class TeamModule { }
