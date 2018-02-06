69ba4cb76067bd7c6e7bb5c49fa642774d7db9ab
dianjuar <dianjuar@gmail.com>
2018-02-05 20:40:58 -0500

    Create the Team Model
    related #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/team.model.ts
@@ -0,0 +1,18 @@
+export interface Team {
+  /**
+   * The name of the team
+   */
+  name: string;
+
+  /**
+   * The flag of the team
+   * Should be and URL
+   */
+  flag: string;
+
+  /**
+   * The shield of the team
+   * Should be and URL
+   */
+  shield: string;
+}
