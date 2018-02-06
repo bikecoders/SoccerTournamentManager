d74428886173757f8a17b39ddcdd08174ff7e10d
dianjuar <dianjuar@gmail.com>
2018-02-06 10:39:14 -0500

    Create the Team List
    Show only flag and name.
    
    Related #4 and #5

--- src/app/shared/element-list/element-list.component.ts
+++ src/app/shared/element-list/element-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Player } from './../../player/shared/player.model';
+import { Team } from './../../team/shared/team.model';
+import { Component, OnInit, Input } from '@angular/core';
+import { Technician } from '../../technician/shared/technician-staff.model';
 
 @Component({
   selector: 'app-element-list',
@@ -7,9 +10,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElementListComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * List of elements to show
+   */
+  @Input() list;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    console.log('Is team?', this.isTeam());
+  }
+
+  /**
+   * Get the name of the element. All our structures has name so me don't
+   * need to check what type of element is
+   *
+   * @param index index of the element to get
+   */
+  getName(index: number) {
+    return this.list[index].name;
+  }
+
+  /**
+   * Get the image of the element.
+   * We need to check what type of element is to know what attribute access
+   *
+   * @param index index of the element to get
+   */
+  getImage(index: number) {
+    // Teams
+    if (this.isTeam()) {
+      return (<Team>this.list[index]).flag;
+    }
+
+    // Player
+    if (this.isTeam()) {
+      return (<Player>this.list[index]).picture;
+    }
+
+    // Technician
+    if (this.isTeam()) {
+      // TODO find a defualt profile picture
+      return '';
+    }
+  }
+
+  /**
+   * Function to know if the element treated here is a Team
+   */
+  private isTeam(): boolean {
+    return !!(<Team>this.list[0]).shield;
+  }
+
+  /**
+   * Function to know if the element treated here is a Player
+   */
+  private isPlayer(): boolean {
+    return !!(<Player>this.list[0]).Position;
+  }
+
+  /**
+   * Function to know if the element treated here is a Technician
+   */
+  private isTechnician(): boolean {
+    return !!(<Technician>this.list[0]).role;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElementListComponent } from './element-list/element-list.component';
 
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatListModule
   ],
   declarations: [ElementListComponent],
   exports: [ElementListComponent]
