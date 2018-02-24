cf40dd051da678f4b5848f69b23c86c30bebddc6
dianjuar <dianjuar@gmail.com>
2018-02-24 00:38:49 -0500

    Create Test for the Service Team
    close #23

--- src/app/teams/shared/teams.service.spec.ts
+++ src/app/teams/shared/teams.service.spec.ts
@@ -1,7 +1,7 @@
-/*
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TeamsService } from './teams.service';
+import { Team } from './team.model';
 
 describe('TeamsService', () => {
   beforeEach(() => {
@@ -13,5 +13,18 @@ describe('TeamsService', () => {
   it('should be created', inject([TeamsService], (service: TeamsService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should current team edited be falsy', inject([TeamsService], (service: TeamsService) => {
+    expect(service.currentTeamEdited).toBeFalsy();
+  }));
+
+  it('should set the current', inject([TeamsService], (service: TeamsService) => {
+    const currentTeam = new Team('Diego', 'flag URL', 'shield URL');
+
+    service.setCurrentTeam(currentTeam);
+
+    expect(service.currentTeamEdited).toBeTruthy('should have something');
+    expect(Object.is(currentTeam, service.currentTeamEdited)).toBeTruthy('Should be the same team');
+  }));
 });
-*/
+
